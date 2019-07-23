e8acbc8052c228fcc899fa202161283591dc9e45
Rene Pfeuffer <rene.pfeuffer@cloudogu.com>
2019-07-23 17:49:41 +0200

    Document suppression

--- src/main/java/com/cloudogu/scmmanager/NotifierProvider.java
+++ src/main/java/com/cloudogu/scmmanager/NotifierProvider.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 public interface NotifierProvider extends ExtensionPoint {
 
-  @SuppressWarnings("squid:S1452")
+  @SuppressWarnings("squid:S1452") // we implement this with a concrete Notifier type to simplify unit tests (no casting)
   Optional<? extends Notifier> get(Run<?, ?> run, ScmInformation information) throws IOException;
 
   static ExtensionList<NotifierProvider> all() {
