2247257560e3150fba5d204760db17de3376a0a6
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-10-06 12:24:03 +0200

    Add better handling of failed ssh authentications

--- src/main/java/com/cloudogu/scmmanager/SSHAuthentication.java
+++ src/main/java/com/cloudogu/scmmanager/SSHAuthentication.java
@@ -28,7 +28,9 @@ public class SSHAuthentication {
       SSHAuthenticator<Connection, StandardUsernameCredentials> authenticator = SSHAuthenticator.newInstance(
         connection, credentials, credentials.getUsername()
       );
-      authenticator.authenticate(new LogTaskListener(LOG, Level.INFO));
+      if (!authenticator.authenticate(new LogTaskListener(LOG, Level.INFO))) {
+        throw new SshConnectionFailedException("ssh authentication failed");
+      }
     } catch (InterruptedException e) {
       Thread.currentThread().interrupt();
       throw new SshConnectionFailedException("failed to authenticate", e);
--- src/main/java/com/cloudogu/scmmanager/SshConnectionFailedException.java
+++ src/main/java/com/cloudogu/scmmanager/SshConnectionFailedException.java
@@ -1,6 +1,11 @@
 package com.cloudogu.scmmanager;
 
 public class SshConnectionFailedException extends RuntimeException {
+
+  public SshConnectionFailedException(String message) {
+    super(message);
+  }
+
   public SshConnectionFailedException(String message, Throwable cause) {
     super(message, cause);
   }
