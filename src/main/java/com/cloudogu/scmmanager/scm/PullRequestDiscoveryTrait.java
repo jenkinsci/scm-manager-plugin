d5da08969f36f9c95f32193aa61c9c168bb7ecf2
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2020-12-02 09:13:59 +0100

    Add option to filter branches with existing pull requests

--- src/main/java/com/cloudogu/scmmanager/scm/PullRequestDiscoveryTrait.java
+++ src/main/java/com/cloudogu/scmmanager/scm/PullRequestDiscoveryTrait.java
@@ -1,9 +1,17 @@
 package com.cloudogu.scmmanager.scm;
 
+import com.cloudogu.scmmanager.scm.api.ScmManagerHead;
+import com.cloudogu.scmmanager.scm.api.ScmManagerPullRequestHead;
+import com.cloudogu.scmmanager.scm.api.ScmManagerTag;
+import com.google.common.annotations.VisibleForTesting;
 import edu.umd.cs.findbugs.annotations.NonNull;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import hudson.Extension;
+import jenkins.scm.api.SCMHead;
 import jenkins.scm.api.SCMHeadCategory;
+import jenkins.scm.api.trait.SCMHeadFilter;
 import jenkins.scm.api.trait.SCMSourceContext;
+import jenkins.scm.api.trait.SCMSourceRequest;
 import jenkins.scm.api.trait.SCMSourceTrait;
 import jenkins.scm.impl.ChangeRequestSCMHeadCategory;
 import jenkins.scm.impl.trait.Discovery;
@@ -13,14 +21,31 @@ import javax.annotation.Nonnull;
 
 public class PullRequestDiscoveryTrait extends SCMSourceTrait {
 
+  private final boolean excludeBranchesWithPRs;
+
   @DataBoundConstructor
+  public PullRequestDiscoveryTrait(boolean excludeBranchesWithPRs) {
+    this.excludeBranchesWithPRs = excludeBranchesWithPRs;
+  }
+
+  /**
+   * Constructor for old versions of this trait, which does not have the excludeBranchesWithPRs option.
+   */
   public PullRequestDiscoveryTrait() {
+    this.excludeBranchesWithPRs = false;
+  }
+
+  public boolean isExcludeBranchesWithPRs() {
+    return excludeBranchesWithPRs;
   }
 
   @Override
   protected void decorateContext(SCMSourceContext<?, ?> context) {
     ScmManagerSourceContext scmContext = (ScmManagerSourceContext) context;
     scmContext.wantPullRequests(true);
+    if (excludeBranchesWithPRs) {
+      scmContext.withFilter(new ExcludePullRequestBranchHeadFilter());
+    }
   }
 
   @Override
@@ -36,6 +61,34 @@ public class PullRequestDiscoveryTrait extends SCMSourceTrait {
     @Override
     public String getDisplayName() {
       return "Pull Request Discovery";
+    }
+  }
+
+  @VisibleForTesting
+  static class ExcludePullRequestBranchHeadFilter extends SCMHeadFilter {
+
+    @Override
+    @SuppressFBWarnings("BC_UNCONFIRMED_CAST") // type is checked isBranchHead
+    public boolean isExcluded(@NonNull SCMSourceRequest request, @NonNull SCMHead head) {
+      if (request instanceof ScmManagerSourceRequest && isBranchHead(head)) {
+        ScmManagerSourceRequest req = (ScmManagerSourceRequest) request;
+        ScmManagerHead branch = (ScmManagerHead) head;
+
+        for (ScmManagerPullRequestHead pullRequest : req.getPullRequests()) {
+          String source = pullRequest.getSource().getName();
+          if (source.equals(branch.getName())) {
+            return true;
+          }
+        }
+
+      }
+      return false;
+    }
+
+    private boolean isBranchHead(@Nonnull SCMHead head) {
+      return head instanceof ScmManagerHead
+        && ! (head instanceof ScmManagerTag)
+        && ! (head instanceof ScmManagerPullRequestHead);
     }
   }
 }
---
+++ src/test/java/com/cloudogu/scmmanager/scm/PullRequestDiscoveryTraitTest.java
@@ -0,0 +1,92 @@
+package com.cloudogu.scmmanager.scm;
+
+import com.cloudogu.scmmanager.scm.api.ScmManagerHead;
+import com.cloudogu.scmmanager.scm.api.ScmManagerPullRequestHead;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import jenkins.scm.api.SCMHead;
+import jenkins.scm.api.trait.SCMSourceRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class PullRequestDiscoveryTraitTest {
+
+  @Mock
+  private ScmManagerSourceContext context;
+
+  @Test
+  public void shouldDecorateContextWithFilter() {
+    PullRequestDiscoveryTrait trait = new PullRequestDiscoveryTrait(true);
+    trait.decorateContext(context);
+
+    verify(context).wantPullRequests(true);
+    verify(context).withFilter(any(PullRequestDiscoveryTrait.ExcludePullRequestBranchHeadFilter.class));
+  }
+
+  @Test
+  public void shouldNotDecorateContextWithFilter() {
+    PullRequestDiscoveryTrait trait = new PullRequestDiscoveryTrait(false);
+    trait.decorateContext(context);
+
+    verify(context).wantPullRequests(true);
+    verify(context, never()).withFilter(any(PullRequestDiscoveryTrait.ExcludePullRequestBranchHeadFilter.class));
+  }
+
+  @Test
+  public void shouldNotExcludeNonScmManagerRequest() {
+    SCMSourceRequest request = mock(SCMSourceRequest.class);
+    SCMHead head = ScmTestData.branch("main");
+
+    assertThat(isExcluded(request, head)).isFalse();
+  }
+
+  @Test
+  public void shouldNotExcludeNonScmManagerHeads() {
+    SCMSourceRequest request = mock(ScmManagerSourceRequest.class);
+    SCMHead head = mock(SCMHead.class);
+
+    assertThat(isExcluded(request, head)).isFalse();
+  }
+
+  @Test
+  public void shouldNotExcludeTags() {
+    SCMSourceRequest request = mock(ScmManagerSourceRequest.class);
+    ScmManagerHead tag = ScmTestData.tag("1.0.0");
+
+    assertThat(isExcluded(request, tag)).isFalse();
+  }
+
+  @Test
+  public void shouldNotExcludePullRequests() {
+    SCMSourceRequest request = mock(ScmManagerSourceRequest.class);
+    ScmManagerHead source = ScmTestData.branch("feature/awesome");
+    ScmManagerHead target = ScmTestData.branch("develop");
+    ScmManagerPullRequestHead pullRequest = ScmTestData.pullRequest("42", target, source);
+
+    assertThat(isExcluded(request, pullRequest)).isFalse();
+  }
+
+  @Test
+  public void shouldExcludeBranchWithOpenPR() {
+    ScmManagerSourceRequest request = mock(ScmManagerSourceRequest.class);
+    ScmManagerHead source = ScmTestData.branch("feature/awesome");
+    ScmManagerHead target = ScmTestData.branch("develop");
+    ScmManagerPullRequestHead pullRequest = ScmTestData.pullRequest("42", target, source);
+
+    when(request.getPullRequests()).thenReturn(Collections.singletonList(pullRequest));
+
+    assertThat(isExcluded(request, source)).isTrue();
+  }
+
+  private boolean isExcluded(@NonNull SCMSourceRequest request, @NonNull SCMHead head) {
+    return new PullRequestDiscoveryTrait.ExcludePullRequestBranchHeadFilter().isExcluded(request, head);
+  }
+}
