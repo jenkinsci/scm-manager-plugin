2e9fc56ab01bfd1f10cecb02ff797f18b1afb6d9
Sebastian Sdorra <sebastian.sdorra@cloudogu.com>
2021-01-18 17:03:12 +0100

    Use ScmManager mercurial browser if available

--- src/main/java/com/cloudogu/scmmanager/scm/ScmManagerHgSCMBuilder.java
+++ src/main/java/com/cloudogu/scmmanager/scm/ScmManagerHgSCMBuilder.java
@@ -2,22 +2,51 @@ package com.cloudogu.scmmanager.scm;
 
 import com.cloudogu.scmmanager.scm.api.ScmManagerHead;
 import com.cloudogu.scmmanager.scm.api.ScmManagerRevision;
+import com.google.common.annotations.VisibleForTesting;
 import edu.umd.cs.findbugs.annotations.NonNull;
 import hudson.plugins.mercurial.MercurialSCMBuilder;
 import hudson.plugins.mercurial.MercurialSCMSource;
+import hudson.plugins.mercurial.browser.HgBrowser;
 import jenkins.scm.api.SCMRevision;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Constructor;
 
 public class ScmManagerHgSCMBuilder extends MercurialSCMBuilder<ScmManagerHgSCMBuilder> {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ScmManagerHgSCMBuilder.class);
 
   public ScmManagerHgSCMBuilder(@NonNull ScmManagerHead head, SCMRevision revision, String credentialsId) {
     super(head, revision, head.getCloneInformation().getUrl(), credentialsId);
 
-    // https://github.com/jenkinsci/mercurial-plugin/pull/146
-    // withBrowser(new ScmManager(head.getCloneInformation().getUrl());
+    HgBrowser browser = findAndCreateBrowser(head.getCloneInformation().getUrl());
+    if (browser != null) {
+       withBrowser(browser);
+    }
 
     if (revision instanceof ScmManagerRevision) {
       withRevision(new MercurialSCMSource.MercurialRevision(head, ((ScmManagerRevision) revision).getRevision()));
     }
+  }
+
+  @VisibleForTesting
+  static HgBrowser findAndCreateBrowser(String url) {
+    return findAndCreateBrowser("hudson.plugins.mercurial.browser.ScmManager", url);
+  }
+
+  @VisibleForTesting
+  static HgBrowser findAndCreateBrowser(String className, String url) {
+    try {
+      Class<? extends HgBrowser> clazz = Class.forName(className).asSubclass(HgBrowser.class);
+      Constructor<? extends HgBrowser> constructor = clazz.getConstructor(String.class);
+      return constructor.newInstance(url);
+    } catch (ClassNotFoundException e) {
+      LOG.info("Could not find ScmManager HgBrowser ({}), please upgrade the mercurial plugin", className);
+    } catch (Exception e) {
+      LOG.warn("failed to create instance of {}", className, e);
+    }
+    return null;
   }
 
 }
---
+++ src/test/java/com/cloudogu/scmmanager/scm/ScmManagerHgSCMBuilderTest.java
@@ -0,0 +1,29 @@
+package com.cloudogu.scmmanager.scm;
+
+import hudson.plugins.mercurial.browser.HgBrowser;
+import hudson.plugins.mercurial.browser.ScmManager;
+import org.junit.Test;
+
+import static com.cloudogu.scmmanager.scm.ScmManagerHgSCMBuilder.findAndCreateBrowser;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScmManagerHgSCMBuilderTest {
+
+  @Test
+  public void shouldReturnNullIfBrowserDoesNotExists() {
+    HgBrowser browser = findAndCreateBrowser("UnknownBrowser", "https://hitchhiker.com");
+    assertThat(browser).isNull();
+  }
+
+  @Test
+  public void shouldReturnForInvalidBrowser() {
+    HgBrowser browser = findAndCreateBrowser("java.lang.Integer", "https://hitchhiker.com");
+    assertThat(browser).isNull();
+  }
+
+  @Test
+  public void shouldReturnScmManagerBrowser() {
+    HgBrowser browser = findAndCreateBrowser("https://hitchhiker.com");
+    assertThat(browser).isInstanceOf(ScmManager.class);
+  }
+}
