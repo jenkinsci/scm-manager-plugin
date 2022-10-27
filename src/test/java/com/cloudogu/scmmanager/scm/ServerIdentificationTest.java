b419c83b41be1fbf53002ee1145cd19e56c0848f
Eduard Heimbuch <eduard.heimbuch@cloudogu.com>
2022-10-27 11:08:23 +0200

    Fix NPE if server url is null

---
+++ src/test/java/com/cloudogu/scmmanager/scm/ServerIdentificationTest.java
@@ -0,0 +1,20 @@
+package com.cloudogu.scmmanager.scm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ServerIdentificationTest {
+
+  @Test
+  public void shouldNotThrowNullPointerExceptionIfServerUrlIsNull() {
+    ServerIdentification serverIdentification = new ServerIdentification(null, null);
+
+    boolean match = serverIdentification.matches(null);
+
+    assertThat(match).isFalse();
+  }
+}
