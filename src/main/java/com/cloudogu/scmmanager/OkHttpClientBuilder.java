3a1704d2345be21d8d2fa8fec9e8446192bd5491
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2023-06-30 11:38:31 +0200

    Add log for failed requests

--- src/main/java/com/cloudogu/scmmanager/OkHttpClientBuilder.java
+++ src/main/java/com/cloudogu/scmmanager/OkHttpClientBuilder.java
@@ -1,11 +1,18 @@
 package com.cloudogu.scmmanager;
 
 import io.jenkins.plugins.okhttp.api.JenkinsOkHttpClient;
+import okhttp3.Call;
+import okhttp3.EventListener;
 import okhttp3.OkHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public final class OkHttpClientBuilder {
+
+  private static final Logger LOG = LoggerFactory.getLogger(OkHttpClientBuilder.class);
 
   private OkHttpClientBuilder() {
   }
@@ -15,6 +22,16 @@ public final class OkHttpClientBuilder {
    * for proxy support and with an increased timeout of one minute.
    */
   public static OkHttpClient build() {
-    return JenkinsOkHttpClient.newClientBuilder(new OkHttpClient()).readTimeout(1L, TimeUnit.MINUTES).build();
+    return
+      JenkinsOkHttpClient
+        .newClientBuilder(new OkHttpClient())
+        .readTimeout(1L, TimeUnit.MINUTES)
+        .eventListener(new EventListener() {
+          @Override
+          public void callFailed(Call call, IOException ioe) {
+            LOG.warn("SCM-Manager request failed", ioe);
+          }
+        })
+        .build();
   }
 }
