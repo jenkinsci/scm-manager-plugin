7d1adc93372c90e61e1a8ad7fd7f3be64cb1e9cb
René Pfeuffer <rene.pfeuffer@cloudogu.com>
2026-07-06 18:39:34 +0200

    Fetch labels for build time

--- src/main/java/com/cloudogu/scmmanager/scm/env/PullRequestLabelsEnvContributor.java
+++ src/main/java/com/cloudogu/scmmanager/scm/env/PullRequestLabelsEnvContributor.java
@@ -1,15 +1,23 @@
 package com.cloudogu.scmmanager.scm.env;
 
+import com.cloudogu.scmmanager.scm.ScmManagerApiData;
+import com.cloudogu.scmmanager.scm.api.PullRequest;
+import com.cloudogu.scmmanager.scm.api.Repository;
+import com.cloudogu.scmmanager.scm.api.ScmManagerApi;
+import com.cloudogu.scmmanager.scm.api.ScmManagerApiFactory;
 import com.cloudogu.scmmanager.scm.api.ScmManagerPullRequestHead;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hudson.EnvVars;
 import hudson.Extension;
 import hudson.model.EnvironmentContributor;
+import hudson.model.ItemGroup;
 import hudson.model.Job;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import javax.annotation.Nonnull;
 import jenkins.branch.Branch;
 import jenkins.scm.api.SCMHead;
@@ -25,6 +33,16 @@ public class PullRequestLabelsEnvContributor extends EnvironmentContributor {
     private static final String ENV_LABEL_PREFIX = "SCMM_PR_LABEL_";
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
+    private final ScmManagerApiFactory apiFactory;
+
+    public PullRequestLabelsEnvContributor() {
+        this(new ScmManagerApiFactory());
+    }
+
+    PullRequestLabelsEnvContributor(ScmManagerApiFactory apiFactory) {
+        this.apiFactory = apiFactory;
+    }
+
     @Override
     public void buildEnvironmentFor(@Nonnull Run run, @Nonnull EnvVars envs, @Nonnull TaskListener listener) {
         ScmManagerPullRequestHead pullRequestHead = getPullRequestHead(run);
@@ -32,7 +50,7 @@ public class PullRequestLabelsEnvContributor extends EnvironmentContributor {
             return;
         }
 
-        List<String> labels = pullRequestHead.getLabels();
+        List<String> labels = fetchLabels(run, pullRequestHead);
         envs.put(ENV_LABELS, toJson(labels));
         envs.put(ENV_LABEL_COUNT, String.valueOf(labels.size()));
 
@@ -55,6 +73,33 @@ public class PullRequestLabelsEnvContributor extends EnvironmentContributor {
         }
 
         return null;
+    }
+
+    private List<String> fetchLabels(Run run, ScmManagerPullRequestHead pullRequestHead) {
+        Job<?, ?> job = run.getParent();
+        ScmManagerApiData apiData = job.getAction(ScmManagerApiData.class);
+        if (apiData == null) {
+            return pullRequestHead.getLabels();
+        }
+
+        try {
+            ScmManagerApi client = apiFactory.create(
+                    (ItemGroup<?>) job.getParent(), apiData.getServerUrl(), apiData.getCredentialsId());
+            Repository repository = client.getRepository(apiData.getNamespace(), apiData.getName())
+                    .get();
+            CompletableFuture<PullRequest> pullRequest = client.getPullRequest(repository, pullRequestHead.getId());
+            if (pullRequest == null) {
+                return pullRequestHead.getLabels();
+            }
+            return pullRequest.get().getLabels();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            log.error("could not fetch labels for pull request {}. Error: {}", pullRequestHead.getId(), e.getMessage());
+        } catch (ExecutionException e) {
+            log.error("could not fetch labels for pull request {}. Error: {}", pullRequestHead.getId(), e.getMessage());
+        }
+
+        return pullRequestHead.getLabels();
     }
 
     private String toJson(List<String> labels) {
--- src/test/java/com/cloudogu/scmmanager/scm/env/PullRequestLabelsEnvContributorTest.java
+++ src/test/java/com/cloudogu/scmmanager/scm/env/PullRequestLabelsEnvContributorTest.java
@@ -1,5 +1,6 @@
 package com.cloudogu.scmmanager.scm.env;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -7,7 +8,12 @@ import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+import com.cloudogu.scmmanager.scm.ScmManagerApiData;
 import com.cloudogu.scmmanager.scm.api.CloneInformation;
+import com.cloudogu.scmmanager.scm.api.PullRequest;
+import com.cloudogu.scmmanager.scm.api.Repository;
+import com.cloudogu.scmmanager.scm.api.ScmManagerApi;
+import com.cloudogu.scmmanager.scm.api.ScmManagerApiFactory;
 import com.cloudogu.scmmanager.scm.api.ScmManagerHead;
 import com.cloudogu.scmmanager.scm.api.ScmManagerPullRequestHead;
 import hudson.EnvVars;
@@ -15,6 +21,7 @@ import hudson.model.TaskListener;
 import hudson.scm.SCM;
 import java.util.List;
 import jenkins.branch.Branch;
+import jenkins.model.Jenkins;
 import jenkins.scm.api.SCMHead;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
@@ -28,7 +35,20 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class PullRequestLabelsEnvContributorTest {
 
+    private static final String SERVER_URL = "http://localhost:8080";
+    private static final String CREDENTIALS_ID = "CREDS_ID";
+    private static final String NAMESPACE = "NAMESPACE";
+    private static final String NAME = "NAME";
     private static final CloneInformation CLONE_INFORMATION = new CloneInformation("git", "http://example.com/repo");
+
+    @Mock
+    private ScmManagerApiFactory apiFactory;
+
+    @Mock
+    private ScmManagerApi api;
+
+    @Mock
+    private Jenkins owner;
 
     @Mock
     private WorkflowJob job;
@@ -49,21 +69,34 @@ class PullRequestLabelsEnvContributorTest {
 
     @BeforeEach
     void beforeEach() {
+        lenient().when(job.getParent()).thenReturn(owner);
         lenient().when(run.getParent()).thenReturn(job);
-        envContributor = new PullRequestLabelsEnvContributor();
+        envContributor = new PullRequestLabelsEnvContributor(apiFactory);
     }
 
     @Test
-    void shouldInjectPullRequestLabels() {
-        useBranch(new ScmManagerPullRequestHead(
-                CLONE_INFORMATION,
-                "42",
-                new ScmManagerHead(CLONE_INFORMATION, "main"),
-                new ScmManagerHead(CLONE_INFORMATION, "feature"),
-                List.of("backend", "needs review", "comma,label")));
+    void shouldInjectCurrentPullRequestLabelsFromApi() {
+        useBranch(pullRequestHead(List.of("old-label")));
+        Repository repository = setupApiCall(pullRequestWithLabels("backend", "needs review", "comma,label"));
 
         envContributor.buildEnvironmentFor(run, envVars, listener);
 
+        verify(api).getPullRequest(repository, "42");
+        verify(envVars).put("SCMM_PR_LABELS", "[\"backend\",\"needs review\",\"comma,label\"]");
+        verify(envVars).put("SCMM_PR_LABEL_COUNT", "3");
+        verify(envVars).put("SCMM_PR_LABEL_1", "backend");
+        verify(envVars).put("SCMM_PR_LABEL_2", "needs review");
+        verify(envVars).put("SCMM_PR_LABEL_3", "comma,label");
+        verifyNoMoreInteractions(envVars);
+    }
+
+    @Test
+    void shouldFallBackToPullRequestHeadLabelsWhenApiDataIsMissing() {
+        useBranch(pullRequestHead(List.of("backend", "needs review", "comma,label")));
+
+        envContributor.buildEnvironmentFor(run, envVars, listener);
+
+        verifyNoInteractions(apiFactory);
         verify(envVars).put("SCMM_PR_LABELS", "[\"backend\",\"needs review\",\"comma,label\"]");
         verify(envVars).put("SCMM_PR_LABEL_COUNT", "3");
         verify(envVars).put("SCMM_PR_LABEL_1", "backend");
@@ -74,14 +107,11 @@ class PullRequestLabelsEnvContributorTest {
 
     @Test
     void shouldInjectEmptyLabelListForPullRequestWithoutLabels() {
-        useBranch(new ScmManagerPullRequestHead(
-                CLONE_INFORMATION,
-                "42",
-                new ScmManagerHead(CLONE_INFORMATION, "main"),
-                new ScmManagerHead(CLONE_INFORMATION, "feature")));
+        useBranch(pullRequestHead(List.of()));
 
         envContributor.buildEnvironmentFor(run, envVars, listener);
 
+        verifyNoInteractions(apiFactory);
         verify(envVars).put("SCMM_PR_LABELS", "[]");
         verify(envVars).put("SCMM_PR_LABEL_COUNT", "0");
         verifyNoMoreInteractions(envVars);
@@ -101,6 +131,31 @@ class PullRequestLabelsEnvContributorTest {
         envContributor.buildEnvironmentFor(run, envVars, listener);
 
         verifyNoInteractions(envVars);
+    }
+
+    private Repository setupApiCall(PullRequest pullRequest) {
+        Repository repository = new Repository(NAMESPACE, NAME, "git");
+        when(job.getAction(ScmManagerApiData.class))
+                .thenReturn(new ScmManagerApiData(SERVER_URL, CREDENTIALS_ID, NAMESPACE, NAME));
+        when(apiFactory.create(owner, SERVER_URL, CREDENTIALS_ID)).thenReturn(api);
+        when(api.getRepository(NAMESPACE, NAME)).thenReturn(completedFuture(repository));
+        when(api.getPullRequest(repository, "42")).thenReturn(completedFuture(pullRequest));
+        return repository;
+    }
+
+    private ScmManagerPullRequestHead pullRequestHead(List<String> labels) {
+        return new ScmManagerPullRequestHead(
+                CLONE_INFORMATION,
+                "42",
+                new ScmManagerHead(CLONE_INFORMATION, "main"),
+                new ScmManagerHead(CLONE_INFORMATION, "feature"),
+                labels);
+    }
+
+    private PullRequest pullRequestWithLabels(String... labels) {
+        PullRequest pullRequest = mock(PullRequest.class);
+        when(pullRequest.getLabels()).thenReturn(List.of(labels));
+        return pullRequest;
     }
 
     private void useBranch(SCMHead head) {
